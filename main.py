fix z_c formula

main.py
@@ -46,11 +46,12 @@ def InverseProjection(point_pixel: tuple) -> tuple:
     v = 2*v0 - v
 
     r12 = R[1][2]
+    r21 = R[2][1]
     r22 = R[2][2]
     ty = t[1][0]
     tz = t[2][0]
 
-    z_c = round((ty*r22-tz*r12)/(((v-v0)*r22)/fy - r12),3)
+    z_c = round((ty*r22+tz*r21)/(((v-v0)*r22)/fy + r21),3)
     x_c = round(((u-u0)/fx) * z_c, 3)
     y_c = round(((v-v0)/fy) * z_c, 3)
     Xc = np.array([x_c, y_c, z_c]).reshape(-1,1)
@@ -71,39 +72,43 @@ def DistanceEstimation(point_pixel: tuple) -> float:
 def main():
     global yolo
 
-    cam = cv.VideoCapture(0)
+    # cam = cv.VideoCapture(0)
 
-    while True:
-        ret, frame = cam.read()
+    # while True:
+    #     ret, frame = cam.read()
 
-        if ret:
-            frame = cv.flip(frame, 1)
+    #     if ret:
+    #         frame = cv.flip(frame, 1)
 
-            results = yolo.predict(frame, conf=0.7)
+    #         results = yolo.predict(frame, conf=0.7)
 
-            if len(results)>0:
-                boxes = results[0].boxes.xyxy.tolist()
+    #         if len(results)>0:
+    #             boxes = results[0].boxes.xyxy.tolist()
 
-                for box in boxes:
-                    pt1 = (int(box[0]), int(box[1]))
-                    pt2 = (int(box[2]), int(box[3]))
+    #             for box in boxes:
+    #                 pt1 = (int(box[0]), int(box[1]))
+    #                 pt2 = (int(box[2]), int(box[3]))
 
-                    cv.rectangle(frame, pt1, pt2, (0,255,0), 1)
+    #                 cv.rectangle(frame, pt1, pt2, (0,255,0), 1)
 
-                    point = (int((pt1[0]+pt2[0])/2), pt2[1])
-                    pred_dis = DistanceEstimation(point_pixel=point)
-                    cv.putText(frame, f'Pred dis: {pred_dis} (m)', (pt1[0],pt1[1]-10), cv.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 1)
+    #                 point = (int((pt1[0]+pt2[0])/2), pt2[1])
+    #                 pred_dis = DistanceEstimation(point_pixel=point)
+    #                 cv.putText(frame, f'Pred dis: {pred_dis} (m)', (pt1[0],pt1[1]-10), cv.FONT_HERSHEY_COMPLEX, 1, (0,255,0), 1)
 
-            cv.imshow('frame', frame)
+    #         cv.imshow('frame', frame)
 
-            key = cv.waitKey(1)
-            if key == ord('q'):
-                break
-        else:
-            break
+    #         key = cv.waitKey(1)
+    #         if key == ord('q'):
+    #             break
+    #     else:
+    #         break
 
-    cam.release()
-    cv.destroyAllWindows()
+    # cam.release()
+    # cv.destroyAllWindows()
+
+    global R, R_inv
+    print(R)
+    print(R_inv)
 
 if __name__ == '__main__':
     main()